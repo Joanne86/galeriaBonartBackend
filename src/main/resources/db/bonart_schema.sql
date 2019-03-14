9a12602d47c663f36b45f83c88f9a7e258ebdf97
leidyyohanarodriguezfuentes <lrodr78@bancodebogota.com.co>
2019-03-14 14:36:11 -0500

    ajuste en bd

--- src/main/resources/db/bonart_schema.sql
+++ src/main/resources/db/bonart_schema.sql
@@ -11,11 +11,11 @@ create table if not exists artist (
     exhibitions varchar (150)
 );
 
-create table if not exists seller {
+create table if not exists seller (
 
     document varchar(20) primary key not null,
     name varchar(45) not null
-};
+);
 
 create table if not exists session (
 	id int (20) primary key auto_increment not null,
@@ -26,17 +26,17 @@ create table if not exists session (
     CONSTRAINT `session_ibfk_1` FOREIGN KEY (document_seller) REFERENCES `seller` (`document`)
 );
 
-create table if not exists room {
+create table if not exists room (
     code int(3) primary key not null,
     name varchar (15) not null,
     document_seller int (20) not null,
     KEY document_seller (document_seller),
     CONSTRAINT `room_ibfk_1` FOREIGN KEY (document_seller) REFERENCES `seller` (`document`)
-};
+);
 
-create table if not exists artwork {
+create table if not exists artwork (
 
-    inscription_code int (3) primary key auto_increment not null,
+    inscription_code int(3) primary key auto_increment not null,
     artwork_type varchar(40) not null,
     price float,
     artist_document varchar (10) not null,
@@ -45,18 +45,18 @@ create table if not exists artwork {
     constraint `artist_ibfk_1` foreign key (artist_document) references `artist` (`document`),
     key number_room (number_room),
     constraint `artist_ibfk_2` foreign key (number_room) references `room` (`number`)
-};
+);
 
-create table if not exists customer {
+create table if not exists customer (
 
-    document varchar (20) primary key not null,
+    document varchar(20) primary key not null,
     name varchar(45) not null,
     cellphone varchar(10) not null,
     email varchar(45) not null,
     entry_cost float not null
-}
+);
 
-create table if not exists sales {
+create table if not exists sales (
 
     code_artwork int (3) not null,
     document_seller int (20) not null,
@@ -64,5 +64,5 @@ create table if not exists sales {
     CONSTRAINT `sales_ibfk_1` FOREIGN KEY (document_seller) REFERENCES `seller` (`document`),
     KEY code_artwork (code_artwork),
     CONSTRAINT `sales_ibfk_2` FOREIGN KEY (code_artwork) REFERENCES `artwork` (`inscription_code`)
-}
+);
 
