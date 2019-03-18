98199959b7699ad21e8fc9d6b88cf09e54554c1f
leidyyohanarodriguezfuentes <lrodr78@bancodebogota.com.co>
2019-03-18 15:44:34 -0500

    ajuste

--- src/main/java/uan/bonart/controller/CustomerController.java
+++ src/main/java/uan/bonart/controller/CustomerController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import uan.bonart.entities.Artwork;
 import uan.bonart.entities.Customer;
 import uan.bonart.service.ICustomerService;
 
