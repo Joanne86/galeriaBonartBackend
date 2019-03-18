d84e218086df050f1e645f1d15b20e07041cd3d0
leidyyohanarodriguezfuentes <lrodr78@bancodebogota.com.co>
2019-03-18 10:48:06 -0500

    creando controladores

---
+++ src/main/java/uan/bonart/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package uan.bonart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uan.bonart.entities.Artwork;
+import uan.bonart.entities.Customer;
+import uan.bonart.service.ICustomerService;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping( "/customer-api" )
+public class CustomerController {
+    @Autowired
+    ICustomerService customerService;
+
+    @GetMapping("/findAll")
+    public ResponseEntity<Iterable<Customer>> findAll() {
+        return new ResponseEntity<>(customerService.findAll(), HttpStatus.OK);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<Customer> create(@RequestBody Customer customer) {
+        System.out.println("etra aqui");
+        return new ResponseEntity<>(customerService.create(customer), HttpStatus.OK);
+    }
+}
