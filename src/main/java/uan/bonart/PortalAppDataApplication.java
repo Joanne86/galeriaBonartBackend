1c9f6cbf4588cd3796bb553d2ad03ec2ede0729d
leidy rodriguez <lrodriguez52@uan.edu.co>
2019-03-18 22:46:00 -0500

    prueba

--- src/main/java/uan/bonart/PortalAppDataApplication.java
+++ src/main/java/uan/bonart/PortalAppDataApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import uan.bonart.service.IArtistService;
+
 @SpringBootApplication
-//@ComponentScan (basePackages = { "uan.bonart."})
+@ComponentScan (basePackageClasses = IArtistService.class)
 
 public class PortalAppDataApplication {
 
