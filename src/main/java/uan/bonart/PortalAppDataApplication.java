49743999f7cf9b1282b339117350e0280c7b028d
leidy rodriguez <lrodriguez52@uan.edu.co>
2019-03-23 21:40:07 -0500

    ajuste main

--- src/main/java/uan/bonart/PortalAppDataApplication.java
+++ src/main/java/uan/bonart/PortalAppDataApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan({""}) 
 public class PortalAppDataApplication {
 
 	public static void main(String[] args) {
