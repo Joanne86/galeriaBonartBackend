7cdbc4922ea12d9a0546a4de87b16f8145ffbda3
leidy rodriguez <lrodriguez52@uan.edu.co>
2019-05-18 22:26:18 -0500

    first unit test

--- src/main/java/uan/bonart/controller/ArtistController.java
+++ src/main/java/uan/bonart/controller/ArtistController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import uan.bonart.entities.Artist;
 import uan.bonart.service.IArtistService;
 
+import java.util.List;
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping( "/artist-api" )
@@ -22,8 +24,8 @@ public class ArtistController {
 		return new ResponseEntity<>("OK", HttpStatus.OK);
 	}
 	@GetMapping("/findAll")
-	public ResponseEntity<Iterable<Artist>> findAll() {
-		return new ResponseEntity<>(artistService.findAll(), HttpStatus.OK);
+	public ResponseEntity<List<Artist>> findAll() {
+		return new ResponseEntity(artistService.findAll(), HttpStatus.OK);
 	}
 	@GetMapping("/findByDocument")
 	public boolean findByDocument(@RequestParam String document) {
--- src/main/java/uan/bonart/repositories/ArtistRepository.java
+++ src/main/java/uan/bonart/repositories/ArtistRepository.java
@@ -1,11 +1,16 @@
 package uan.bonart.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import uan.bonart.entities.Artist;
 import uan.bonart.entities.Artwork;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArtistRepository extends CrudRepository<Artist, Integer> {
     Optional<Artist> findByDocument(String document);
+
+    @Query ("SELECT a FROM Artist a")
+    List<Artist> findAll();
 }
